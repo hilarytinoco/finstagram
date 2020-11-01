78d244634b2ab3df2454be44f3b1539dcb53ae1f
hilarytinoco <hilaryt.tinoco@gmail.com>
2020-11-01 22:50:14 +0000

    adds routes for comments and likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :finstagram_post
+    has_many :comment_likes
+
+    validates_presence_of :text, :user, :finstagram_post
 end
+ def like_count
+    self.likes.size
+  end
